375291f53b824766d8fee29ba56640992038d49c
Henry <henry.tran06@sjsu.edu>
2015-07-15 02:34:40 -0700

    Fixed dependency path for memory.lua

--- animexport.lua
+++ animexport.lua
@@ -5,7 +5,6 @@
 
 --[[
 Copyright (c) 2015 Henry Tran
-
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the
 "Software"), to deal in the Software without restriction, including
@@ -13,10 +12,8 @@ without limitation the rights to use, copy, modify, merge, publish,
 distribute, sublicense, and/or sell copies of the Software, and to
 permit persons to whom the Software is furnished to do so, subject to
 the following conditions:
-
 The above copyright notice and this permission notice shall be included
 in all copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
 OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -26,7 +23,7 @@ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ]]
 
-run("./libs/memory.lua")
+run("./samples_2.4/libs/memory.lua")
 
 arg = memory.load({xOffset = 0, yOffset = 0, spacing = 0, maxRow = 0})
 
